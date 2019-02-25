a07039a2c5c1f2e4549e5edb61ac44a216710e3a
Nick <ntm112294@gmail.com>
2019-02-24 19:59:51 -0500

    added new url for adding items to an existing list and updated FTs

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -64,13 +64,14 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        self.fail('Finish the test!')
-
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effectself.
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url,'/lists/.+')
 
         # She visists that URL - her to-do list is still thereself.
+        self.browser.get(edith_list_url)
 
         # Satisfied, she goes back to sleep
 
